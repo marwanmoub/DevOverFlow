2bd9f724fdb4669107e6dc20b5a91e43653cdf9b
marwanmoub <moubayedmarwancoding@gmail.com>
2025-01-29 00:48:18 +0200

    add dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <h1 className="text-3xl text-white font-black">Welcome to the world of Next.js</h1>
+    <h1 className="text-3xl dark:text-white font-black">
+      Welcome to the world of Next.js
+    </h1>
   );
-}
+};
 
 export default Home;
