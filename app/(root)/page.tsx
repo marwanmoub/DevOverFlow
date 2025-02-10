89ba39bd25d5b1ba8ad65311bd71639cb5943f8f
marwanmoub <moubayedmarwancoding@gmai.com>
2025-02-10 10:12:18 -0800

    Add home filters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import Link from "next/link";
 
 const questions = [
   {
@@ -23,7 +25,7 @@ const questions = [
     title: "How to learn JavaScript?",
     description: "I want to learn JavaScript, can anyone help me?",
     tags: [
-      { _id: "1", name: "React" },
+      { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
     ],
     author: { _id: "1", name: "John Doe" },
@@ -35,43 +37,47 @@ const questions = [
 ];
 
 interface SearchParams {
-  searchParams: Promise<{[key: string]: string}>
+  searchParams: Promise<{ [key: string]: string }>;
 }
 
-const Home = async ({searchParams}: SearchParams) => {
-
-  const {query = ""} = await searchParams;
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
-    return question.title.toLowerCase().includes(query.toLowerCase())
-  })
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
-      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">
-          All Questions      
-        </h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-        <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900" asChild>
-          <Link href={ROUTES.ASK_QUESTION}>
-            Ask a Question
-          </Link>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-
       <section className="mt-11">
-        <LocalSearch route="/" imgSrc='/icons/search.svg' placeholder="Search questions..." otherClasses="flex-1" />
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
       </section>
-
-      HomeFilter
-
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {
-          filteredQuestions.map((question) => (
-            <h1 key={question._id}>{question.title}</h1>
-          ))
-        }
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
     </>
   );
