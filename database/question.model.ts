dd45d0817c888b7912ee2a98749455b048228f56
marwanmoub <moubayedmarwancoding@gmail.com>
2025-02-22 04:37:37 -0800

    update on question model

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,13 +1,14 @@
-import { models, ObjectId, Schema, model } from "mongoose";
+import { models, ObjectId, Schema, model, Types } from "mongoose";
 
 export interface IQuestion {
   title: string;
   content: string;
+  tags: Types.ObjectId[];
   views: number;
   answers: number;
   upvotes: number;
   downvotes: number;
-  author: ObjectId;
+  author: Types.ObjectId;
   createdAt: Date;
 }
 
@@ -15,6 +16,7 @@ const QuestionSchema = new Schema<IQuestion>(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
     views: { type: Number, required: true, default: 0 },
     answers: { type: Number, required: true, default: 0 },
     upvotes: { type: Number, required: true, default: 0 },
