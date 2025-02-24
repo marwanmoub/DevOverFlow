ea52817ac0c1c73a9d5aa2f85c75e19090525017
marwanmoub <moubayedmarwancoding@gmai.com>
2025-02-24 04:25:33 -0800

    Implement logging

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["pino", "pino-pretty"],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '',
-        port: ""
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
