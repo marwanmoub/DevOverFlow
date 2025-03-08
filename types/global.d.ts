d229876c1deceb94d00ee47e6cbd05be26a8c572
marwanmoub <moubayedmarwancoding@gmail.com>
2025-03-08 05:55:36 -0800

    implement crud operations for users

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,21 +1,41 @@
+import { NextResponse } from "next/server";
+
 interface Tag {
-    _id: string;
-    name: string;
+  _id: string;
+  name: string;
 }
-  
+
 interface Author {
-    _id: string;
-    name: string;
-    image: string;
+  _id: string;
+  name: string;
+  image: string;
 }
-  
+
 export interface Question {
-    _id: string;
-    title: string;
-    tags: Tag[];
-    author: Author;
-    createdAt: Date;
-    upvotes: number;
-    answers: number;
-    views: number;
+  _id: string;
+  title: string;
+  tags: Tag[];
+  author: Author;
+  createdAt: Date;
+  upvotes: number;
+  answers: number;
+  views: number;
 }
+
+export type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+export type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+export type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+export type APIErrorResponse = NextResponse<ErrorResponse>;
+export type APIResponse<T = null> = NextResponse<
+  SuccessResponse<T> | ErrorResponse
+>;
