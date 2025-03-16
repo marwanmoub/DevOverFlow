405707262ecd91fbf94679efd7700cd774fcf6b5
marwanmoub <moubayedmarwancoding@gmail.com>
2025-03-16 06:41:42 -0700

    Add oauth sign in

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,11 +1,12 @@
 const ROUTES = {
-    HOME: '/',
-    SIGN_IN: '/sign-in',
-    SIGN_UP: '/sign-up',
-    ASK_QUESTION: '/ask-a-question',
-    QUESTION: (id: string) => `/question/${id}`,
-    PROFILE: (id: string) => `/profile/${id}`,
-    TAGS: (id: string) => `/tags/${id}`,
+  HOME: "/",
+  SIGN_IN: "/sign-in",
+  SIGN_UP: "/sign-up",
+  ASK_QUESTION: "/ask-a-question",
+  QUESTION: (id: string) => `/question/${id}`,
+  PROFILE: (id: string) => `/profile/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
+  SIGN_IN_WITH_OAUTH: "sign-in-with-oauth",
 };
 
 export default ROUTES;
--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,9 @@
 import { IUser } from "@/database/user.model";
 import { fetchHandler } from "./handlers/fetch";
 import { IAccount } from "@/database/account.model";
+import { SignInWithOAuthParams } from "@/types/action";
+import { ActionResponse } from "@/types/global";
+import ROUTES from "@/constants/routes";
 
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
@@ -51,5 +54,17 @@ export const api = {
       fetchHandler(`${API_BASE_URL}/accounts/${id}`, {
         method: "DELETE",
       }),
+  },
+  auth: {
+    oAuthSignIn: ({
+      user,
+      provider,
+      providerAccountId,
+    }: SignInWithOAuthParams) => {
+      return fetchHandler(`${API_BASE_URL}/auth/${ROUTES.SIGN_IN_WITH_OAUTH}`, {
+        method: "POST",
+        body: JSON.stringify({ user, provider, providerAccountId }),
+      });
+    },
   },
 };
