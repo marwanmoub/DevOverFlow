6889361b051546d6e6402b8358e9911dc5de3341
marwanmoub <moubayedmarwancoding@gmai.com>
2025-04-04 14:20:49 -0700

    Implement edit question

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -22,16 +22,23 @@ import { z } from "zod";
 import TagCard from "../cards/TagCard";
 import { useRouter } from "next/navigation";
 import handleError from "@/lib/handlers/error";
-import { createQuestion } from "@/lib/actions/question.action";
+import { createQuestion, editQuestion } from "@/lib/actions/question.action";
 import { toast } from "@/hooks/use-toast";
 import ROUTES from "@/constants/routes";
+import { QuestionInterface } from "@/types/global";
+import { Params } from "next/dist/server/request/params";
 
 const Editor = dynamic(() => import("@/components/editor"), {
   // Make sure we turn SSR off
   ssr: false,
 });
 
-const QuestionForm = () => {
+interface Params {
+  question?: QuestionInterface;
+  isEdit?: boolean;
+}
+
+const QuestionForm = ({ question, isEdit = false }: Params) => {
   const editorRef = useRef<MDXEditorMethods>(null);
   const [isPending, startTransition] = React.useTransition();
 
@@ -40,9 +47,9 @@ const QuestionForm = () => {
   const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
-      title: "",
-      content: "",
-      tags: [],
+      title: question?.title || "",
+      content: question?.content || "",
+      tags: question?.tags.map((tag) => tag.name) || [],
     },
   });
 
@@ -51,6 +58,29 @@ const QuestionForm = () => {
   ) => {
     startTransition(async () => {
       try {
+        if (isEdit && question) {
+          const result = await editQuestion({
+            questionId: question?._id,
+            ...data,
+          });
+
+          if (result.success) {
+            toast({
+              title: "Question Updated",
+              description: "Your question has been updated successfully.",
+            });
+
+            if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+          } else {
+            toast({
+              title: "Error",
+              description: result.error?.message || "Something went wrong",
+              variant: "destructive",
+            });
+          }
+
+          return;
+        }
         const result = await createQuestion(data);
 
         if (result.success) {
@@ -223,7 +253,7 @@ const QuestionForm = () => {
                 <span>Sumitting</span>
               </>
             ) : (
-              <>Ask A Question</>
+              <>{isEdit ? "Edit" : "Ask a Question"}</>
             )}
           </Button>
         </div>
